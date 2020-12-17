e7a6d244483b5064c6cad622b42e4caa7993cccf
Olle Lögdahl <olle.logdahl@gmail.com>
2020-12-17 11:39:20 +0100

    Added media

--- examples/ex.c
+++ examples/ex.c
@@ -6,10 +6,21 @@ IHCT_TEST(arithmetic_addition_basic) {
     IHCT_ASSERT(8 + 8 == 16);
 }
 IHCT_TEST(arithmetic_addition_big) {
-    IHCT_NASSERT(1000 + 1 == 1000);
+    IHCT_NASSERT(1000 + 1 == 1001);
 }
 IHCT_TEST(arithmetic_multiplication) {
     IHCT_ASSERT(1 * 3 == 4);
+}
+
+IHCT_TEST(timeout_slow) {
+    for(;;) {
+        int i = 1;
+    }
+}
+
+IHCT_TEST(sigsegv_test) {
+    int *p = NULL;
+    *p = 3;
 }
 
 IHCT_TEST(strings_basic) {
@@ -27,11 +38,6 @@ IHCT_TEST(strings_more) {
     IHCT_ASSERT_STR("ccc", "ccc");
     IHCT_ASSERT_STR("ddd", "ddd");
     IHCT_ASSERT_STR("eee", "eee");
-}
-
-IHCT_TEST(sigsegv_test) {
-    int *p = NULL;
-    *p = 3;
 }
 
 IHCT_TEST(timeout_test) {
